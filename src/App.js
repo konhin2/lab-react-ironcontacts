1a47118c0758b1df931f3061ff9d7e80361b5702
Konhin CM <maskmesta@gmail.com>
2021-08-03 17:48:05 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
+import contacts from './contacts.json'
 
 function App() {
+  // IMPORTACIONES
+  const [ state, setState ] = useState(contacts.slice(0,5))
+    
+  // FUNCIONES
+  const getRandom = (event) => {
+    event.preventDefault()
+    const filteredArray = contacts.filter((contact) => {
+      return !state.find((c) => c.id === contact.id)
+    })
+    setState(
+      [...state, filteredArray[Math.floor(Math.random() * filteredArray.length)]]
+    )
+  }
+  // SORT
+  const sortedName = (event) => {
+    event.preventDefault()
+    const sortedArray = [...state.sort((a, b) => a.name.localeCompare(b.name))]
+    setState(
+      sortedArray
+    )
+  }
+  const sortedPopularity = (event) => {
+    event.preventDefault()
+    const filteredContactsArray = [...state]
+    filteredContactsArray.sort((a, b)=>{
+      return b.popularity - a.popularity
+    })
+    setState(filteredContactsArray)
+  }
+  // Delete
+  const deteleContact = (event, element) => {
+    event.preventDefault()
+    const filteredArray = state.filter((e) => {
+      return e.id !== element.id
+    })
+    setState(filteredArray)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <table className="table">
+        <caption>IronContacts</caption>
+        <button onClick={(e) => getRandom(e)}>Add Random Contact</button>
+        <button onClick={(e) => sortedName(e)}>Sorted By Name</button>
+        <button onClick={(e) => sortedPopularity(e)}>Sorted By Popularity</button>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Action</th>
+        </tr>
+        {
+          state.map(contact => {
+            return (
+              <tr>
+                <td><img src={contact.pictureUrl} className='img-size'/></td>
+                <td>{contact.name}</td>
+                <td>{contact.popularity.toFixed(2)}</td>
+                <td>
+                  <button className="delete" onClick={(e) => {deteleContact(e, contact)}}>Delete</button>
+                </td>
+              </tr>
+            )
+          })
+        }
+      </table>
     </div>
   );
 }
